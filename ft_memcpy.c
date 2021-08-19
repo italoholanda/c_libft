6b5292ef611d071cb10016c45a82c95ba95dc038
italo holanda <italogholanda@gmail.com>
2021-08-19 18:06:56 -0300

    memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/19 17:47:48 by igomes-h          #+#    #+#             */
+/*   Updated: 2021/08/19 18:06:34 by igomes-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*memcpy(void *dest, const void *src, size_t n)
+{
+	int		i;
+	char	*new_dest;
+	char	*new_src;
+
+	i = 0;
+	new_dest = (unsigned char *)dest;
+	new_src = (unsigned char *)src;
+	while(i < n)
+	{
+		new_dest[i] = new_src[i];
+		++i;
+	}
+}
