f0acab01b6fc1df845e6d2b0fb45bd2aaa6b2d81
italo holanda <italogholanda@gmail.com>
2021-08-23 11:05:34 -0300

    fixed memcpy and memmove created

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,28 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/19 17:47:48 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/08/23 10:28:29 by igomes-h         ###   ########.fr       */
+/*   Created: 2021/08/20 09:13:10 by igomes-h          #+#    #+#             */
+/*   Updated: 2021/08/23 10:28:12 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+
+void	*ft_memmove(void *dest,	const void *src, size_t	n)
 {
-	int				i;
+	int		i;
 	unsigned char	*new_dest;
 	unsigned char	*new_src;
+	char	buffer[n];
 
 	i = 0;
 	new_dest = (unsigned char *)dest;
 	new_src = (unsigned char *)src;
 	while(i < n)
 	{
-		new_dest[i] = new_src[i];
+		buffer[i] = new_src[i];
+		++i;
+	}
+	i = 0;
+	while(i < n)
+	{
+		new_dest[i] = buffer[i];
 		++i;
 	}
 	return(dest);
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 11:00:16 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/08/20 09:03:30 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/08/20 09:16:54 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int		ft_isdigit(int c);
 void	ft_bzero(void *mem, size_t n);
 void	*ft_memset(void *mem, int byte, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest,	const void *src, size_t	n);
 size_t	ft_strlen(const char *str);
 
 #endif
