d2f00a70d10412df2340913de05b5f5fef9236e2
Ítalo <italogholanda@gmail.com>
2021-09-02 13:28:46 -0300

    fixed strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,22 +6,26 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 11:50:52 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/08/30 12:54:23 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/09/02 13:23:10 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	char	*str;
 	char	*address;
 
+	if (!c)
+		return ((char *)s + ft_strlen(s));
 	address = 0;
 	str = (char *)s;
 	while (*str)
 	{
 		if (*str == c)
 			address = str;
-		++str;
+		str++;
 	}
 	return (address);
 }
