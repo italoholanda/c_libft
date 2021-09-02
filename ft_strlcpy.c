ef62649737e415fbfd7c6ae8b27a5fed7fe73e36
Ítalo <italogholanda@gmail.com>
2021-09-02 15:07:49 -0300

    fixed strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 09:54:43 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/08/31 18:00:03 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/09/02 15:05:50 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	unsigned int	i;
 
 	i = 0;
-	while (src[i] && i < size - 1)
+	if (size != 0)
 	{
-		dst[i] = src[i];
-		++i;
+		while (src[i] && i < size - 1)
+		{
+			dst[i] = src[i];
+			++i;
+		}
+		dst[i] = 0;
 	}
-	dst[i] = 0;
-	return (ft_strlen(dst));
+	return (ft_strlen(src));
 }
