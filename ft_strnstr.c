c2ee5c6f7ec70fe5c93c5b3add5b7edaeb77c42f
Ítalo <italogholanda@gmail.com>
2021-09-02 18:20:00 -0300

    fixed strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,33 +6,31 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 17:39:51 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/09/01 18:30:01 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/09/02 16:56:02 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big_s, const char *short_s, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
-	size_t	j;
-	size_t	short_len;
-	char	*new_big;
+	size_t	little_len;
 
 	i = 0;
-	new_big = (char *)big_s;
-	short_len = ft_strlen(short_s);
-	if (short_len || big_s == short_s)
-		return (new_big);
-	while (new_big[i] && i < len)
+	little_len = ft_strlen(little);
+	if (!*little || !*big || (*big == *little))
+		return ((char *)big);
+	while (len && (*big && i < len))
 	{
-		j = 0;
-		while (new_big[i + j] && short_s[j]
-			&& new_big[i + j] == short_s[j] && i + j < len)
-			++j;
-		if (j == short_len)
-			return (new_big + i);
-		++i;
+		if (ft_strncmp((char *)big, (char *)little, little_len) == 0)
+		{
+			return ((char *)big);
+		}
+		big++;
+		i++;
+		if (i + little_len > len)
+			return (NULL);
 	}
-	return (0);
+	return (NULL);
 }
