f6895b6bdc65b4b01a4542c44735c4676cad8cc7
Ítalo <italogholanda@gmail.com>
2021-09-02 09:23:01 -0300

    fixed strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpm.c                                       :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 13:00:18 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/08/30 13:10:41 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/09/02 09:22:30 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
+	unsigned char	*u_s1;
+	unsigned char	*u_s2;
 
 	i = 0;
-	while ((s1[i] || s2[i]) && i < n)
+	u_s1 = (unsigned char *)s1;
+	u_s2 = (unsigned char *)s2;
+	while ((u_s1[i] || u_s2[i]) && i < n)
 	{
-		if (s1[i] < s2[i])
+		if (u_s1[i] < u_s2[i])
 			return (-1);
-		if (s1[i] > s2[i])
+		if (u_s1[i] > u_s2[i])
 			return (1);
 		i++;
 	}
