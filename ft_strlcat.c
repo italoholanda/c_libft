599e63529daa6529480449470efd7274d6f93aad
Ítalo <italogholanda@gmail.com>
2021-09-03 14:38:14 -0300

    partial fix strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 10:42:24 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/08/31 17:52:34 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/09/03 14:34:07 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,27 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	unsigned int	i;
-	unsigned int	j;
+	char	*new_src;
+	size_t	dst_size;
+	size_t	src_size;
+	size_t	amount;
+	size_t	i;
 
-	i = ft_strlen(dst);
-	j = 0;
-	while (src[j] && j < size)
+	new_src = (char *)src;
+	dst_size = ft_strlen(dst);
+	src_size = ft_strlen(new_src);
+	amount = 0;
+	i = 0;
+	if (size > dst_size)
+		amount = src_size + dst_size;
+	else
+		amount = src_size + size;
+	while (new_src[i] && (dst_size + 1) < size)
 	{
-		dst[i + j] = src[i];
-		j++;
+		dst[dst_size] = new_src[i];
+		dst_size++;
+		i++;
 	}
-	return (ft_strlen(src) + (i + j));
+	dst[dst_size] = 0;
+	return (amount);
 }
