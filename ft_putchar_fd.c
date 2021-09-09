d25c469fdb0e48d74b2b79ec24d3887c99429525
Ítalo <italogholanda@gmail.com>
2021-09-09 11:51:33 -0300

    putchar_fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/20 09:13:10 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/09/08 20:37:48 by igomes-h         ###   ########.fr       */
+/*   Created: 2021/09/09 11:39:01 by igomes-h          #+#    #+#             */
+/*   Updated: 2021/09/09 11:42:57 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	ft_putchar_fd(char c, int fd)
 {
-	size_t	i;
-
-	if (!dest && !src)
-		return (0);
-	i = 0;
-	if (dest < src)
-		ft_memcpy(dest, src, n);
-	else
-	{
-		i = n - 1;
-		while ((int)i >= 0)
-		{
-			((char *)dest)[i] = ((char *)src)[i];
-			i--;
-		}
-	}
-	return (dest);
+	write(fd, &c, 1);
 }
