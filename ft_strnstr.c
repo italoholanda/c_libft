a750ccb02c5261a1beea30f6e136b30eca9c789e
Ítalo <italogholanda@gmail.com>
2021-09-11 10:54:40 -0300

    fixed strnstr structure

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,31 +6,30 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 17:39:51 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/09/11 10:46:37 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/09/11 10:53:37 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#include "libft.h"
+
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	i;
 	size_t	little_len;
+	size_t	i;
 
-	if (!*little || !*big || (*big == *little))
+	if (!(*little) || !(*big) || (ft_strlen(big) < len))
 		return ((char *)big);
 	i = 0;
 	little_len = ft_strlen(little);
-	while (len && (*big && i < len))
+	while (*big && (i < len))
 	{
-		if (ft_strncmp((char *)big, (char *)little, little_len) == 0)
-		{
+		if ((ft_strncmp(big, little, little_len) == 0)
+			&& (i + little_len <= len))
 			return ((char *)big);
-		}
 		big++;
 		i++;
-		if (i + little_len > len)
-			return (0);
 	}
 	return (0);
 }
