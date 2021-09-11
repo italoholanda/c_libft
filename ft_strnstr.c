aea12aa7d8fc0b7887a6cb66de6aeb48e2d2b733
Ítalo <italogholanda@gmail.com>
2021-09-11 10:47:54 -0300

    fix strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 17:39:51 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/09/02 16:56:02 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/09/11 10:46:37 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	i;
 	size_t	little_len;
 
-	i = 0;
-	little_len = ft_strlen(little);
 	if (!*little || !*big || (*big == *little))
 		return ((char *)big);
+	i = 0;
+	little_len = ft_strlen(little);
 	while (len && (*big && i < len))
 	{
 		if (ft_strncmp((char *)big, (char *)little, little_len) == 0)
@@ -30,7 +30,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 		big++;
 		i++;
 		if (i + little_len > len)
-			return (NULL);
+			return (0);
 	}
-	return (NULL);
+	return (0);
 }
