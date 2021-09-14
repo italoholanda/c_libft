b84efde0063a9854f626fe5bc1f9dd98da6ea5b7
Ítalo <italogholanda@gmail.com>
2021-09-14 12:21:08 -0300

    strrchar 0 protection

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 11:50:52 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/09/14 12:17:34 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/09/14 12:20:54 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ char	*ft_strrchr(const char *s, int c)
 	char	*address;
 	char	char_c;
 
-	char_c = (char)c;
-	if (!char_c)
+	char_c = (unsigned char)c;
+	if (char_c == 0)
 		return ((char *)s + ft_strlen(s));
 	address = 0;
 	str = (char *)s;
