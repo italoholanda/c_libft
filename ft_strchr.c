7f361a0999ab77ccfbd91372be2aa4544f078361
Ítalo <italogholanda@gmail.com>
2021-09-14 12:05:39 -0300

    strchr casting

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 11:50:52 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/08/30 11:52:23 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/09/14 12:05:14 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ char	*ft_strchr(const char *s, int c)
 	char	*str;
 
 	str = (char *)s;
-	while (*str != c)
+	while (*str != (char)c)
 	{
 		if (!*str)
 			return (0);
